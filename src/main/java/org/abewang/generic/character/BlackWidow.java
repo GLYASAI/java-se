b73c9b63ef8b260bd4e1afc61c89fd4638d86b81
GLYASAI <chaoschunqfeijg@163.com>
2018-05-12 15:44:14 +0800

    泛型接口

---
+++ src/main/java/org/abewang/generic/character/BlackWidow.java
@@ -0,0 +1,8 @@
+package org.abewang.generic.character;
+
+/**
+ * @Author Abe
+ * @Date 2018/5/12.
+ */
+public class BlackWidow extends StoryCharacters {
+}
---
+++ src/main/java/org/abewang/generic/character/Spiderman.java
@@ -0,0 +1,8 @@
+package org.abewang.generic.character;
+
+/**
+ * @Author Abe
+ * @Date 2018/5/12.
+ */
+public class Spiderman extends StoryCharacters {
+}
